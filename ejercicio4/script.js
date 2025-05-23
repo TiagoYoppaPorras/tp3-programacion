1dade7a440fcc1f81c369362e0ec0dc1f49fa58c
tiagoYoppaPorras <tiagoyoppa@gmail.com>
2025-05-23 20:29:08 -0300

    creando carpetas fusiones sin conflicto

--- ejercicio4/script.js
+++
@@ -1,8 +0,0 @@
-function calcularIMC() {
-  const estatura = parseFloat(document.getElementById('estatura').value);
-  const peso = parseFloat(document.getElementById('peso').value);
-
-  const imc = peso / (estatura * estatura);
-  alert("Tu IMC es: " + imc.toFixed(2));
-}
-
